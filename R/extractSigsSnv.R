b7dde4e1336dedee9157e6a378e069220a5a88af
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-05-07 12:57:47 +0200

    - Replaced stringr functions for base string manipulation functions - Changed some variable names - Added progress messages

--- R/extractSigsSnv.R
+++ R/extractSigsSnv.R
@@ -17,24 +17,29 @@
 #' installed.
 #' @param signature.profiles A matrix containing the mutational signature profiles, where rows are
 #' the mutation contexts and the columns are  the mutational signatures.
+#' @param verbose Print progress messages?
 #'
 #' @return A 1-column matrix
 #' @export
-extractSigsSnv <- function(vcf.file, output = 'signatures', sample.name = NULL,
-                           ref.genome = DEFAULT_GENOME, signature.profiles = SNV_SIGNATURE_PROFILES, ...){
+extractSigsSnv <- function(
+   vcf.file, output = 'signatures', sample.name = NULL,
+   ref.genome = DEFAULT_GENOME, signature.profiles = SNV_SIGNATURE_PROFILES,
+   verbose=F, ...
+){
+   variants <- variantsFromVcf(vcf.file, mode = 'snv', ref.genome, verbose=verbose, ...)
+   # variants <- variantsFromVcf(
+   #    vcf.file='/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/ICGC/vcf/BRCA-EU/snv_indel/PD10010_snv_indel.vcf.gz',
+   #    mode = 'snv', ref.genome
+   # )
 
-   #vcf.file = '/Users/lnguyen/hpc/cog_bioinf/cuppen/project_data/Luan_projects/CHORD/scripts_main/mutSigExtractor/R_source/call_snv_signatures/EGAZ00001222791_EGAZ00001222814_post_processed.vcf.gz'
-   #output = 'contexts'
-   variants <- variantsFromVcf(vcf.file, mode = 'snv', ref.genome, ...)
-   #variants <- variantsFromVcf(vcf.file, mode = 'snv', ref.genome)
+   if(verbose){ message('Initializing SNV signature output vector...') }
+   context_counts <- structure(
+      rep(0, length(SUBS_CONTEXTS_96)),
+      names=SUBS_CONTEXTS_96
+   )
 
-   ## Deal with empty vcfs
-   if(!is.data.frame(variants) && is.na(variants)){
-      subs_context_counts <- rep(0, length(SUBS_CONTEXTS_96))
-      names(subs_context_counts) <- SUBS_CONTEXTS_96
-   }
-
-   else {
+   if(is.data.frame(variants) & !is.na(variants)){ ## Don't process empty vcfs
+      if(verbose){ message('Converting trinucleotide contexts to substitution contexts...') }
       ## Get trinucleotide contexts that don't conform to C>N or T>N
       select_opp_types <- which(!(variants$substitution %in% SUBSTITUTIONS))
 
@@ -43,7 +48,7 @@ extractSigsSnv <- function(vcf.file, output = 'signatures', sample.name = NULL,
          chartr('ATGC', 'TACG', variants[select_opp_types,'tri_context'])
       )
 
-      ## Single nt can simply be complemented (no need for reverse)
+      ## Single nt can simply be complemented (no need to reverse)
       variants[select_opp_types,'substitution'] <- chartr('ATGC', 'TACG', variants[select_opp_types,'substitution'])
 
       ## Convert trinucleotide context to substitution context
@@ -53,31 +58,26 @@ extractSigsSnv <- function(vcf.file, output = 'signatures', sample.name = NULL,
          substr(variants$tri_context, 3, 3)
       )
 
-      ## Count context occurrences. Fill unfound contexts with 0
-      subs_context_counts <- table(variants$subs_context)
-
-      subs_context_counts <- unlist(lapply(SUBS_CONTEXTS_96, function(i){
-         counts <- subs_context_counts[i]
-         if(is.na(counts)){ counts <- 0 }
-         return(counts)
-      }))
-
-      names(subs_context_counts) <- SUBS_CONTEXTS_96
+      ## Count context occurrences. Fill found contexts into context_counts
+      if(verbose){ message('Counting substitution context occurrences...') }
+      context_counts_new <- table(variants$subs_context)
+      context_counts[names(context_counts_new)] <- context_counts_new
    }
 
    ## Export
    if(output == 'contexts'){
-      out <- as.matrix(subs_context_counts)
+      if(verbose){ message('Returning context counts...') }
+      out <- as.matrix(context_counts)
 
    } else if(output == 'signatures'){
+      if(verbose){ message('Returning absolute signature contributions...') }
       ## Least squares fitting
-      out <- fitToSignatures(signature.profiles, subs_context_counts)$x
+      out <- fitToSignatures(signature.profiles, context_counts)$x
       names(out) <- colnames(signature.profiles)
       out <- as.matrix(out)
    }
 
-   if(is.null(sample.name)){ colnames(out) <- basename(vcf.file) }
-   else { colnames(out) <- sample.name }
+   colnames(out) <- if(is.null(sample.name)){ basename(vcf.file) } else { sample.name }
 
    return(out)
 }
