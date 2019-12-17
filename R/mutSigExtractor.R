d768e7cff998550570c36ca0e8dd59382c470b42
Luan Nguyen <nguyen.n.luan777@gmail.com>
2019-12-17 19:05:43 +0100

    hg19 genome no longer a dependency. If BSgenome.Hsapiens.UCSC.hg19 exists, it is loaded on package startup

--- R/mutSigExtractor.R
+++ R/mutSigExtractor.R
@@ -1,7 +1,24 @@
 ## Global variables
 
+#--------- Auto-load hg19 ref genome if it exists ----------#
 DEFAULT_GENOME <- 'BSgenome.Hsapiens.UCSC.hg19'
 
+.onLoad <- function(libname, pkgname){
+   if(DEFAULT_GENOME %in% rownames(installed.packages())){
+      message('Loading the default reference genome: ', DEFAULT_GENOME)
+      do.call('library', list(DEFAULT_GENOME))
+   } else {
+      warning("
+         No reference genome loaded. Please install and/or load a BSgenome. For example:
+
+         install.packages('BiocManager')
+         BiocManager::install('BSgenome.Hsapiens.UCSC.hg19')
+         library('BSgenome.Hsapiens.UCSC.hg19')
+      ")
+   }
+}
+
+#--------- SNV context types ----------#
 SUBSTITUTIONS <- c('C>A','C>G','C>T','T>A','T>C','T>G')
 
 C_TRIPLETS <- c(
@@ -19,4 +36,5 @@ T_TRIPLETS <- c(
 CONTEXTS_96 <- c(rep(C_TRIPLETS, 3), rep(T_TRIPLETS, 3))
 SUBSTITUTIONS_96 <- rep(SUBSTITUTIONS, each=16)
 SUBS_CONTEXTS_96 <- paste0(substr(CONTEXTS_96,1,1), "[", SUBSTITUTIONS_96, "]", substr(CONTEXTS_96,3,3))
+
 
