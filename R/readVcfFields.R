d69a050de80e63e5eaea1e6f9c3f1c075cf3455e
Luan Nguyen <nguyen.n.luan777@gmail.com>
2020-01-15 09:37:41 +0100

    Added INFO field parsing function getInfoValues()

--- R/readVcfFields.R
+++ R/readVcfFields.R
@@ -1,4 +1,4 @@
-#' A basic vcf reader
+#' Read vcf into R as data frame
 #'
 #' @param vcf.file Path to the vcf file
 #' @param fields A character or integer vector indicating which columns to keep.
@@ -30,3 +30,31 @@ readVcfFields <- function(vcf.file, fields=NULL){
    return(vcf)
 }
 
+####################################################################################################
+#' Get values from INFO field
+#'
+#' @param v A character vector of the INFO field, with each item being a line of the INFO field
+#' @param keys A character vector of the names of the INFO field values to retrieve
+#'
+#' @return A character matrix containing the key names and corresponding values
+#' @export
+getInfoValues <- function(v, keys){
+   #v=vcf$info
+   l <- strsplit(v,';')
+   l <- lapply(l, function(i){
+      do.call(rbind,strsplit(i,'='))
+   })
+
+   out <- do.call(rbind,lapply(l, function(i){
+      #i=l[[1]]
+      v <- i[match(keys, i[,1]),2]
+      if(length(v)==0){
+         return(rep(NA,length(keys)))
+      }
+      return(v)
+   }))
+
+   colnames(out) <- keys
+
+   return(as.data.frame(out, stringsAsFactors=F))
+}
